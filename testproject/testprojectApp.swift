ba046169fdd5b4464e4322c9bc1b6de51cb2eedd
Darcy Liu <darcy.ldx@gmail.com>
2025-10-20 00:29:41 -0400

    Initial Commit

---
+++ testproject/testprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testprojectApp.swift
+//  testproject
+//
+//  Created by Darcy Liu on 2025-10-20.
+//
+
+import SwiftUI
+
+@main
+struct testprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
